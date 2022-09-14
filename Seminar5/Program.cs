4546d5f0024be9ae16b9edde48b57b3e8799d6d1
Aleksey Smirnov <acwork@list.ru>
2022-09-14 22:39:58 +0300

    Add Example_1

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,1 +1,77 @@
 ﻿
+System.Console.WriteLine("Введите количество чисел в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число - нижняя граница интервала");
+int mingen = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число - нижняя граница интервала");
+int maxgen = Convert.ToInt32(Console.ReadLine());
+
+int[] RandMas ( int numbers, int minnumbers,int maxnumbers )
+
+{
+    int[] arr = new int [numbers];
+    int length = arr.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(minnumbers, maxnumbers+1);
+        
+    }
+return  arr;
+}
+
+int[] arr = RandMas(n, mingen, maxgen);
+
+int[] SumDigit (int[]array)
+
+
+    {
+       int length  = array.Length; 
+       int possum = 0;
+       int negsum = 0;
+        
+      
+        for (int i = 0; i < length; i++)
+            {   
+                if (array[i] > 0)
+                    {
+                       
+                        possum+= array[i]; 
+                    }   
+            
+                else 
+                    {
+                        negsum+=array[i];
+                    }
+            
+            }
+
+            int[] result = new int[2];
+            result[0] = possum;
+            result[1] = negsum;
+
+            return result;    
+
+    }
+
+void PrintResult(int[] array, int[] maxminvalue)
+
+
+    {
+         var length = array.Length;
+         for (int i = 0; i < length; i++)
+         {
+            System.Console.Write($"{arr[i]} ");
+         }
+
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Сумма положительныч чисел {maxminvalue[0]}");
+        System.Console.WriteLine($"Сумма отрицательных чисел {maxminvalue[1]}");
+
+    }
+
+int[] result = SumDigit(arr);
+
+PrintResult(arr, result);
+           
+
